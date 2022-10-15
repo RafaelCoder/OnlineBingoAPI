fa8176f222f3b767b1922e2945c41748ed1c28ed
Rafael William Ribeiro <rwribeiro.php@gmail.com>
2022-10-15 16:41:58 -0300

    added an index page

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineBingoAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hi Everyone!";
+        }
+    }
+}
